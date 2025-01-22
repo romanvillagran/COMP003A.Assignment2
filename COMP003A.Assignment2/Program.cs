02f2733f8b7dfb490540b24f2e2eacf15a0dffa0
Your Name <you@example.com>
2025-01-22 00:50:34 -0800

    added the author, course, faculty, and purpose

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace COMP003A.Assignment2
+﻿//Author: Roman
+//Course: comp003a
+//Faculty: Jonathan
+//Purpose: assignment
+namespace COMP003A.Assignment2
 {
     internal class Program
     {
