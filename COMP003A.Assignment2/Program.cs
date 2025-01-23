b2e2ed70973e4da3d0486f48771f1ba3b10bfe9b
Your Name <you@example.com>
2025-01-22 17:21:33 -0800

    added some stuff like my future age and more

--- COMP003A.Assignment2/Program.cs
+++ COMP003A.Assignment2/Program.cs
@@ -8,7 +8,40 @@ namespace COMP003A.Assignment2
     {
         static void Main(string[] args)
         {
+
+            string userFirstName;
+            string userLastName;
+            int userAge;
+            double productPrice;
+            bool isStudent;
+
+           
+ 
             
+            Console.Write("Enter your first name");
+            userFirstName = Console.ReadLine();
+
+            Console.Write("Enter your last name");
+            userLastName = Console.ReadLine();
+            
+            Console.Write("Enter your age: ");
+            userAge = int.Parse(Console.ReadLine());
+
+            
+            Console.Write("Enter the price of a product");
+            productPrice = double.Parse(Console.ReadLine());
+
+            
+            Console.Write(" Are you a student? (true/false)");
+            isStudent = bool.Parse(Console.ReadLine());
+
+            int futureAge = userAge + 5;
+
+
+            Console.WriteLine($"\nHello, {userFirstName} {userLastName}!");
+            Console.WriteLine($"Your current age is {userAge}.");
+            Console.WriteLine($"In 5 years, you will be {futureAge}");
+
         }
     }
 }
